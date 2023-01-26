01a1ed8157761641bde39e4d8dc8ce0d41bc03fb
Artem <artkrasov@gmail.com>
2023-01-26 19:56:03 +0300

    Changed the output of the product and made access to the data via singleton

--- KrasovPizzaModel/ApplicationContext.cs
+++ KrasovPizzaModel/ApplicationContext.cs
@@ -4,6 +4,19 @@ namespace KrasovPizzaModel;
 
 internal class ApplicationContext : DbContext
 {
+
+    private static ApplicationContext _context;
+    private ApplicationContext()
+    {
+        
+    }
+
+    public static ApplicationContext GetContext()
+    {
+        _context ??= new ApplicationContext();
+        return _context;
+    }
+
     public DbSet<Product> Products { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
